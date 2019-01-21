4114bbb8f24aff0c5f6fe13c79f1ac53a70f83c3
chenhongju <864196621@qq.com>
2019-01-21 22:05:30 +0800

    提交ALGO 13

---
+++ src/algo/ALGO_13.java
@@ -0,0 +1,62 @@
+package algo;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class ALGO_13 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[] missile = reader.readLine().split(" ");
+        reader.close();
+        int[] high = new int[missile.length];
+        for (int i = 0; i < high.length; i++) {
+            high[i] = Integer.parseInt(missile[i]);
+        }
+        System.out.println(getMaxIntercept(high));
+        System.out.println(getNumberOfIntercepter(high));
+    }
+
+    //求最长下降子序列长度
+    private static int getMaxIntercept(int[] high) {
+        int[] dp = new int[high.length];
+        int max = 0;
+        for (int i = 0; i < high.length; i++) {
+            for (int j = i + 1; j < high.length; j++) {
+                if (high[j] <= high[i]) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                    max = Math.max(max, dp[j]);
+                }
+            }
+        }
+        return max + 1;
+    }
+
+    
+    private static int getNumberOfIntercepter(int[] high) {
+        int numberOfIntercepter = 0;
+        boolean[] isIntercept = new boolean[high.length];
+        for (int i = 0; i < high.length; i++) {
+            int midHighDifference = getMinHighDifference(high, i, isIntercept);
+            if (midHighDifference == -1) {
+                numberOfIntercepter++;
+            } else {
+                isIntercept[midHighDifference] = true;
+            }
+        }
+        return numberOfIntercepter;
+    }
+
+    private static int getMinHighDifference(int[] high, int pos, boolean[] isIntercept) {
+        int minHighDifferenceIndex = -1;
+        for (int i = 0; i < pos; i++) {
+            if (!isIntercept[i] && high[i] >= high[pos]) {
+                if (minHighDifferenceIndex == -1 || high[i] < high[minHighDifferenceIndex]) {
+                    minHighDifferenceIndex = i;
+                }
+            }
+        }
+        return minHighDifferenceIndex;
+    }
+}
