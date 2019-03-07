0627ed90f7ff6fee70cf1b4fa44a56ccdef578e9
chenhongju <864196621@qq.com>
2019-03-07 23:49:36 +0800

    update

--- src/java_5/b_6.java
+++ src/java_5/b_6.java
@@ -1,5 +1,52 @@
 package java_5;
 
-public class b_6 {
+import org.omg.PortableInterceptor.INACTIVE;
 
+/*
+ 标题：奇怪的分式
+
+    上小学的时候，小明经常自己发明新算法。一次，老师出的题目是：
+
+    1/4 乘以 8/5
+
+    小明居然把分子拼接在一起，分母拼接在一起，答案是：18/45 （参见图1.png）
+
+    老师刚想批评他，转念一想，这个答案凑巧也对啊，真是见鬼！
+
+    对于分子、分母都是 1~9 中的一位数的情况，还有哪些算式可以这样计算呢？
+
+    请写出所有不同算式的个数（包括题中举例的）。
+
+    显然，交换分子分母后，例如：4/1 乘以 5/8 是满足要求的，这算做不同的算式。
+
+    但对于分子分母相同的情况，2/2 乘以 3/3 这样的类型太多了，不在计数之列!
+
+注意：答案是个整数（考虑对称性，肯定是偶数）。请通过浏览器提交。不要书写多余的内容。
+ */
+public class b_6 {
+	public static void main(String args[]) {
+		int sum = 0;
+		for(int a=1;a<10;a++){
+			for(int b=0;b<10;b++){
+				if(a==b)continue;
+				for (int c = 1; c < 10; c++) {
+					for(int d=1;d<10;d++){
+						if(c==d)continue;
+						//求最大公约数
+						int n_1 = gcd(a*c,b*d);
+						int n_2 = gcd(a*10+c, b*10+d);
+						if (a*c/n_1==(a*10+c)/n_2&&b*d/n_1==(b*10+d)/n_2) {
+							sum++;
+						}
+					}
+				}
+			}
+		}
+		System.out.println(sum);
+	}
+	
+	static int gcd(int a,int b){
+		if (b==0)return a;
+		return gcd(b, a%b);
+	}
 }
